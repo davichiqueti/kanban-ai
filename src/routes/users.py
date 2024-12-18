89a36a44b2340eb415b232339ab4cfd9de2e46ff
Davi Chiqueti <davi.chiqueti@hooklab.com.br>
2024-12-18 15:11:19 -0300

    feat: user route for different users

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,12 +1,19 @@
 from fastapi import APIRouter, Depends
 from models_ import User, UserPublic
-from sqlmodel import select
+from sqlmodel import select, Session
 from utils.jwt_authentication import get_current_user
+from database import get_session
 
 
 router = APIRouter()
 
 
 @router.get("/users/me", response_model=UserPublic)
-async def get_user(user = Depends(get_current_user)):
+async def get_user(user=Depends(get_current_user)):
+    return user
+
+
+@router.get("/users/{username}", response_model=UserPublic)
+async def get_user(username: str, session: Session = Depends(get_session), _=Depends(get_current_user)):
+    user = session.exec(select(User).filter(User.username == username)).first()
     return user
