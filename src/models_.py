13382dbc7910dfaa741e8f662405b133a8ff97e1
Davi Chiqueti <davi.chiqueti@hooklab.com.br>
2024-12-18 15:11:33 -0300

    feature: board cards

--- src/models_.py
+++ src/models_.py
@@ -19,7 +19,7 @@ class User(SQLModel, table=True):
     password: str
     board_links: list["BoardUserLink"] = Relationship(back_populates="user")
     name: str
-    username: str = Field(unique=True)
+    username: str = Field(unique=True, index=True)
     email: str = Field(unique=True)
     active: bool = Field(default=True)
     created_at: datetime = Field(default_factory=datetime.now)
@@ -29,10 +29,11 @@ class User(SQLModel, table=True):
 class Board(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
-    description: Optional[str]
+    description: Optional[str] = None
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now, sa_column_kwargs={"onupdate": datetime.now})
     user_links: list["BoardUserLink"] = Relationship(back_populates="board")
+    cards: list["BoardCard"] = Relationship(back_populates="board")
 
 
 class BoardUserRole(str, Enum):
@@ -51,6 +52,34 @@ class BoardUserLink(SQLModel, table=True):
     )
     board: Board = Relationship(back_populates="user_links")
     user: User = Relationship(back_populates="board_links")
+
+
+class BoardCardStatus(str, Enum):
+    BACKLOG = "backlog"
+    TO_DO = "to do"
+    DOING = "doing"
+    REVIEW  = "review"
+    DONE = "done"
+
+class BoardCardPriority(Enum):
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+
+
+class BoardCard(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    description: Optional[str] = None
+    status: BoardCardStatus = BoardCardStatus.BACKLOG
+    priority: Optional[BoardCardPriority]
+    created_at: datetime = Field(default_factory=datetime.now)
+    due_date: Optional[datetime] = None
+    updated_at: datetime = Field(default_factory=datetime.now, sa_column_kwargs={"onupdate": datetime.now})
+    board_id: int = Field(default=None, foreign_key="board.id")
+    board: Board = Relationship(back_populates="cards")
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
+
 
 class UserPublic(UserBase):
     id: int
--- src/routes/boards.py
+++ src/routes/boards.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
-from models_ import Board, BoardUserLink, BoardUserRole, User
+from models_ import Board, BoardUserLink, BoardUserRole, BoardCardPriority, BoardCardStatus, User
 from database import get_session
 from sqlmodel import Session, select
 from utils.jwt_authentication import get_current_user
 from typing import Optional
 from pydantic import BaseModel
+from datetime import datetime
 
 
 router = APIRouter()
@@ -15,13 +16,28 @@ class BoardCreate(BaseModel):
     description: Optional[str]
 
 
+class BoardCardCreate(BaseModel):
+    title: str
+    description: Optional[str] = None
+    status: BoardCardStatus = BoardCardStatus.BACKLOG
+    priority: Optional[BoardCardPriority]
+    created_at: datetime = Field(default_factory=datetime.now)
+    due_date: Optional[datetime] = None
+    updated_at: datetime = Field(default_factory=datetime.now, sa_column_kwargs={"onupdate": datetime.now})
+    board_id: int = Field(default=None, foreign_key="board.id")
+    board: Board = Relationship(back_populates="cards")
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")()
+
 class BoardResponse(BaseModel):
     id: int
     name: str
     description: Optional[str]
+    created_at: datetime
+    user_links: list
+    cards: list
 
 
-@router.get("/boards")
+@router.get("/boards", response_model=list[BoardResponse])
 async def get_user_boards(session: Session = Depends(get_session), user=Depends(get_current_user)):
     boards = session.exec(
         select(Board)
@@ -47,10 +63,32 @@ async def create_board(board: BoardCreate, session: Session = Depends(get_sessio
     return new_board
 
 
-@router.put("/boards/{board_id}/heroes/{user_id}", response_model=BoardResponse)
+@router.post("/boards/{board_id}/cards", response_model=BoardResponse)
+async def add_board_member(board_id: int, card: session: Session = Depends(get_session), current_user=Depends(get_current_user)):
+    board = session.exec(
+        select(Board)
+        .join(BoardUserLink)
+        .filter(
+            Board.id == board_id,
+            BoardUserLink.user_id == current_user.id
+        )
+    ).first()
+    if (not board):
+        raise HTTPException(status_code=404, detail="Board not found")
+    board_user_link = BoardUserLink(
+        board_id=board.id,
+        user_id=user.id,
+        role=BoardUserRole.COLLABORATOR
+    )
+    session.add(board_user_link)
+    session.commit()
+    return board
+
+
+@router.put("/boards/{board_id}/user/{username}", response_model=BoardResponse)
 async def add_board_member(
     board_id: int,
-    user_id: int,
+    username: str,
     session: Session = Depends(get_session),
     current_user=Depends(get_current_user)
 ):
@@ -63,8 +101,8 @@ async def add_board_member(
             BoardUserLink.role == BoardUserRole.OWNER
         )
     ).first()
-    user = session.get(User, user_id)
-    if not board or not user:
+    user = session.exec(select(User).filter(User.username == username)).first()
+    if (not board or not user):
         raise HTTPException(status_code=404, detail="Board or User not found")
     board_user_link = BoardUserLink(
         board_id=board.id,
