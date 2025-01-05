98a218259cc02fcb5f9abd66792d02b6e7c1068f
Davi Chiqueti <davichiqueti@gmail.com>
2025-01-05 19:50:05 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import create_db_and_tables
 from routes.auth import router as auth_router
 from routes.users import router as users_router
 from routes.boards import router as boards_router
+import os
 
 
 app = FastAPI(root_path="/api")
@@ -10,3 +12,11 @@ app.add_event_handler("startup", create_db_and_tables)
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(boards_router)
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.environ["FRONTEND_URL"]],  # Adjust this to your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
