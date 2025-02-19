23e2b46b05b185fe3ac05f98fb529cf055e8a79c
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-02-19 19:25:08 -0300

    user logout btn

--- frontend/components/header/headerDropdown.tsx
+++ frontend/components/header/headerDropdown.tsx
@@ -1,4 +1,7 @@
+
 import { User } from "./header";
+
+import { useRouter } from "next/navigation";
 
 interface HeaderInfoDropdownProps {
     userInfo: User | null;
@@ -6,10 +9,18 @@ interface HeaderInfoDropdownProps {
 }
 
 export default function HeaderDropdown({ userInfo, setModalOpen }: HeaderInfoDropdownProps) {
+    const router  = useRouter()
+
+    const handleLogout = () => {
+
+        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        router.push("/auth")
+    }
+
     return (
-        <div 
+        <div
             className="absolute right-0 w-64 max-w-xs bg-white border border-gray-300 rounded-lg shadow-lg p-4 flex flex-col gap-2"
-            onMouseEnter={() => setModalOpen(true)} 
+            onMouseEnter={() => setModalOpen(true)}
             onMouseLeave={() => setModalOpen(false)}
         >
             <h2 className="text-lg font-bold whitespace-nowrap overflow-hidden text-ellipsis">
@@ -19,7 +30,8 @@ export default function HeaderDropdown({ userInfo, setModalOpen }: HeaderInfoDro
                 {userInfo?.email}
             </h3>
 
-            <button 
+            <button
+                onClick={handleLogout}
                 className="mt-2 w-full bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
             >
                 Logout
--- frontend/lib/services/boardServices.ts
+++ frontend/lib/services/boardServices.ts
@@ -4,9 +4,7 @@ import api from "./api"
 export const createBoard = async (boardData: {name: string, description: string}) => {
 
     try{
-
         const response = await api.post("/boards", boardData)
-        console.log("Board criado ", response.data)
         return response.data
 
     }catch(error){
@@ -17,9 +15,7 @@ export const createBoard = async (boardData: {name: string, description: string}
 export const getMyBoards = async () => {
 
     try{
-
         const response = await api.get("/boards")
-        console.log("Meus boards ", response.data)
         return response.data
 
     }catch(error){
