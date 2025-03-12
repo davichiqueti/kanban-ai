04d805dcd1df6242992b95f8f9f3ab5bd415c11e
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-03-12 15:46:02 -0300

    update board description

--- frontend/app/(private)/board/[id]/page.tsx
+++ frontend/app/(private)/board/[id]/page.tsx
@@ -91,7 +91,6 @@ export default function BoardPage() {
         <div>
           {isEditingName ? (
             <div>
-
               <input
                 type="text"
                 value={name}
@@ -120,7 +119,6 @@ export default function BoardPage() {
               </div>
             </div>
           ) : (
-
             <div className="flex flex-row">
               <h1 className="text-2xl font-bold">{board.name}</h1>
               <button
@@ -131,24 +129,62 @@ export default function BoardPage() {
               </button>
             </div>
           )}
-
         </div>
 
-        <p className="text-gray-600">{board.description}</p>
+        <div>
+          {isEditingDescription ? (
+            <div>
+              <input
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className=" p-1 mb-2 border-2 rounded w-full outline-none focus:border-blue-500 "
+                autoFocus
+              />
+
+              <div className="flex gap-4">
+                <button
+                  className="py-1 px-3 bg-blue-500 border rounded-lg text-white"
+                  onClick={() => {
+                    handleUpdateBoardInfo("description", description);
+                    setIsEditingDescription(false);
+                    setDescription("")
+                  }}
+                >Save</button>
+
+                <button
+                  className="py-1 px-3 bg-slate-200 border rounded-lg"
+                  onClick={() => {
+                    setIsEditingDescription(false);
+                    setDescription("")
+                  }}
+                >Cancel</button>
+              </div>
+            </div>
+          ) : (
+            <div className="flex flex-row">
+              <p className="text-gray-600">{board.description}</p>
+              <button
+                onClick={() => setIsEditingDescription(true)}
+                className="ml-2 mr-2  flex items-center opacity-0 transition-opacity duration-300  hover:opacity-100 hover:bg-slate-200 p-1 rounded"
+              >
+                <AiOutlineEdit />
+              </button>
+            </div>
+          )}
+        </div>
+
         <p className="text-xs text-gray-400">
           Created at: {new Date(board.created_at).toLocaleDateString()}
         </p>
 
       </div>
 
-
       <div className="flex-grow overflow-auto">
-
         <KanbanBoard board={board} onBoardChange={handleBoardChange} />
-
       </div>
 
     </div>
-  );
 
+  );
 }
