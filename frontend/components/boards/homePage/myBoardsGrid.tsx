8819f8e7f44fb9af4a5d5d446b15ee28002a1726
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-03-13 15:39:44 -0300

    boards order

--- frontend/components/boards/homePage/myBoardsGrid.tsx
+++ frontend/components/boards/homePage/myBoardsGrid.tsx
@@ -16,31 +16,35 @@ export default function MyBoardsGrid({ boards, onNewBoard }: MyBoardsGridprops) 
         router.push(`/board/${id}`);
     };
 
+    console.log(boards)
+
 
     return (
         <div>
             <div className="mb-4">
-                <CreateBoardButton  onNewBoard={onNewBoard}/>
+                <CreateBoardButton onNewBoard={onNewBoard} />
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {boards.length > 0 ? (
-                    boards.map((board) => (
-                        <div
-                            key={board.id}
-                            onClick={() => handleBoardClick(board.id)}
-                            className="p-4 bg-blue-200 border border-blue-700 rounded-lg 
+                    [...boards]
+                        .sort((a, b) => a.id - b.id)
+                        .map((board) => (
+                            <div
+                                key={board.id}
+                                onClick={() => handleBoardClick(board.id)}
+                                className="p-4 bg-blue-200 border border-blue-700 rounded-lg 
                     shadow-md hover:scale-105 transition delay-75"
-                        >
+                            >
 
-                            <h2 className="font-bold text-lg">{board.name}</h2>
-                            <p className="text-gray-500">{board.description}</p>
-                            <p className="text-xs text-gray-400">
-                                Criado em: {new Date(board.created_at).toLocaleDateString()}
-                            </p>
+                                <h2 className="font-bold text-lg">{board.name}</h2>
+                                <p className="text-gray-500">{board.description}</p>
+                                <p className="text-xs text-gray-400">
+                                    Criado em: {new Date(board.created_at).toLocaleDateString()}
+                                </p>
 
-                        </div>
-                    ))
+                            </div>
+                        ))
                 ) : (
                     <p className="text-gray-500 text-center col-span-3">Carregando boards.</p>
                 )}
